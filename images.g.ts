62f3ba17a1f96babd0e3217d748d685dbb6dc555
nilgonzalez07 <106734349+nilgonzalez07@users.noreply.github.com>
2022-06-02 11:00:52 +0200

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Llupis":return img`
+. . . . f f f f f . . . . . . . 
+. . . f 1 1 1 1 1 f . . . . . . 
+. . f d d d d 1 1 1 f . . . . . 
+. f d f d d f d 1 1 f f . . . . 
+. f d f d d f d 1 1 d d f . . . 
+f d 1 1 d d d d 1 1 f d f . . . 
+f d d d d f d d 1 1 f d f . f f 
+f f f f f d d d 1 1 f f . f 1 f 
+. f d d d d d 1 1 f f . . f 1 f 
+. . f f f f f 1 1 1 1 f . f 1 f 
+. . . . f 1 1 1 1 1 1 1 f f 1 f 
+. . . f 1 f f 1 f 1 1 1 1 f f . 
+. . . f 1 f f 1 f 1 1 1 1 f . . 
+. . . f d f f d f f f 1 f . . . 
+. . . f d d f d d f f d f . . . 
+. . . . f f f f f f f f f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
